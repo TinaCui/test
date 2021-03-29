243e5ef4d83b4334fa07ae559a655611640b6033
cuidongqin <cuidongqin@mininglamp.com>
2021-03-29 15:42:34 +0800

    test

---
+++ js/getterAndSetter.js
@@ -0,0 +1,34 @@
+/**
+ * 功能：实现一个getter方法,获取指定字符串key的值。
+ * 举例：
+ * const a = {b:{c:{d:2}}}
+ * getter(a, 'b.c.d');
+ *
+ * const a = {b: [{c:3}]}
+ * getter(a, 'b[0].c')
+ *
+ * 考虑字符串key更复杂的情况，比如：'b[0].a[1][2].c[1]'
+ * @param obj
+ * @param keyStr
+ */
+function getter(obj, keyStr) {
+
+}
+
+
+
+/**
+ * 功能：实现一个setter函数，为给定的对象修改指定字符串key的值
+ * 举例：
+ * setter(n, "a.b.c.d", 3);
+ * console.log(n.a.b.c.d); //3
+ * setter(n, "a.b.bx", 1);
+ * console.log(n.a.b.bx); //1
+ * @param obj 给定的对象
+ * @param keyStr  字符串key
+ * @param value   值
+ */
+
+function setter(obj, keyStr, value) {
+
+}
--- js/microsoft.js
+++ js/microsoft.js
@@ -1,14 +1,14 @@
 /**
- * 给定一个数组，和一个target，求数组中两个数相加等于target，返回这两个数的数组下标。
+ * 给定一个数组，和一个target，求数组中两个数相加等于target，返回这两个数的下标。
  * @param arr
  * @param target
  * @return {Array}
  */
-function getTargetIndexs(arr, target){
+function getTargetIndexs(arr, target) {
     let indexes = [];
     for (let i = 0; i < arr.length; i++) {
         let other = target - arr[i];
-        for (let j = i+1; j < arr.length; j++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] === other) {
                 indexes.push(i);
                 indexes.push(j);
@@ -18,29 +18,29 @@ function getTargetIndexs(arr, target){
     return indexes;
 }
 
-function getTargetIndexs(arr, target){
+function getTargetIndexs(arr, target) {
     let map = {}
     for (let i = 0; i < arr.length; i++) {
         let other = target - arr[i];
         map[arr[i]] = i;
-        if(map[other]) {
+        if (map[other]) {
             return [i, map[other]]
         }
     }
 }
 
 // [1,2,3,4,5,6] target = 10
-function getTargetIndexs(arr, target){
-    arr.sort((a,b)=>{
+function getTargetIndexs(arr, target) {
+    arr.sort((a, b) => {
         return a - b;
     });
     let i = 0, j = arr.length - 1;
     while (i != j) {
         let sum = arr[i] + arr[j];
 
-        if ( sum === target) {
+        if (sum === target) {
             return [i, j];
-        }else {
+        } else {
             sum < target ? i++ : j--;
         }
     }
